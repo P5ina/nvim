421f816c859c1f1fab2600e92fde50ae5d7a3cfa
Timur Turatbekov <timatur242005@gmail.com>
2024-02-14 01:32:45 +0300

    Fixed lsp bugs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -52,49 +52,21 @@ require("lazy").setup({
     -- LSP Support
     {
         'williamboman/mason.nvim',
-        config = function()
-            require('mason').setup()
-        end,
     },
     {
         'williamboman/mason-lspconfig.nvim',
-        config = function()
-            local lsp_zero = require('lsp-zero')
-            require('mason-lspconfig').setup({
-                handlers = {
-                    lsp_zero.default_setup,
-                },
-                ensure_installed = {
-                    'lua_ls',
-                }
-            })
-        end,
     },
     {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
         lazy = true,
-        config = function()
-            local lsp_zero = require('lsp-zero')
-
-            lsp_zero.on_attach(function(_, bufnr)
-                -- see :help lsp-zero-keybindings
-                -- to learn the available actions
-                lsp_zero.default_keymaps({buffer = bufnr})
-            end)
-        end,
+        config = false,
     },
     {
         'neovim/nvim-lspconfig',
         dependencies = {
             {'hrsh7th/cmp-nvim-lsp'},
         },
-        config = function ()
-            local lsp_zero = require('lsp-zero')
-            lsp_zero.setup({
-                'lua_ls'
-            })
-        end,
     },
     -- Autocompletion
     {
@@ -102,17 +74,42 @@ require("lazy").setup({
         dependencies = {
             {'L3MON4D3/LuaSnip'}
         },
-        config = function()
-            local cmp = require('cmp')
-
-            cmp.setup({
-                mapping = cmp.mapping.preset.insert({
-                    ['<Tab>'] = cmp.mapping.confirm(),
-                }),
-                completion = {
-                    completeopt = 'menu,menuone,noinsert',
-                },
-            })
-        end,
     },
+})
+
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(_, bufnr)
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    handlers = {
+        lsp_zero.default_setup,
+    },
+    ensure_installed = require('plugins.lsp-servers'),
+})
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+        -- `Enter` key to confirm completion
+        ['<Tab>'] = cmp.mapping.confirm({ select = false }),
+
+        -- Ctrl+Space to trigger completion menu
+        ['<C-Space>'] = cmp.mapping.complete(),
+
+        -- Navigate between snippet placeholder
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+        -- Scroll up and down in the completion documentation
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    })
 })
