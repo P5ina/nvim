a50fb12bc8e4b746f052af96fd4fe38d256d75b0
Timur Turatbekov <timatur242005@gmail.com>
2026-01-15 09:41:21 +0600

    Add vimtree

--- lua/p5ina/remap.lua
+++ lua/p5ina/remap.lua
@@ -1,7 +1,6 @@
 local wk = require("which-key")
 local mappings = {
   { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
-  { "<leader>e", vim.cmd.Ex, desc = "Open file explorer", mode = "n" },
 }
 
 wk.add(mappings)
